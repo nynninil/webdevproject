71babec5455ccd20791ffa4146d714b90e1ee41a
nynninil <kulthida138@gmail.com>
2024-09-29 03:17:38 +0700

    update gitignore

--- CGM48FANS/urls.py
+++ CGM48FANS/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 
 CGM48FANS = ''
 urlpatterns = [
-    path('', views.page1, name='Home'),
-    path('members/', views.members, name='members'),
+    # path('', views.page1, name='Home'),
+    path('', login_required(views.page1), name='Home'),
+    path('members/', login_required(views.members), name='members'),
     # path('informations/', views.members, name='members'),
-    path('song/', views.song, name='song'),
+    path('song/', login_required(views.song), name='song'),
     path('signup/',views.signup, name='signup'),
     path('signin/',views.signin, name='signin')   
 ]
--- CGM48FANS/views.py
+++ CGM48FANS/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
 from django.contrib import messages
-from django import forms
-from .forms import SignUpForm, SignInForm,UserCreationForm
+from .forms import SignUpForm
 from .models import Member2, Album, Single
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/signin/')
 def page1(request):
     try:
         # Attempt to get all members
@@ -18,7 +17,8 @@ def page1(request):
     
     return render(request, 'home/index.html', {'members': members})
 
-# Create your views here.
+
+@login_required(login_url='/signin/')
 def members(request):
     try:
         # Attempt to get all members
@@ -30,6 +30,7 @@ def members(request):
     return render(request, 'member/index.html', {'members': members})
 
 # Create your views here.
+@login_required(login_url='/signin/')
 def song(request):
     try:
         # Attempt to get all members
@@ -41,7 +42,7 @@ def song(request):
     return render(request, 'song/index.html', {'members': members})
 
 
-
+@login_required(login_url='/signin/')
 def song(request):
     import pprint
     albums = Album.objects.prefetch_related('list_songs').all()  # Fetch all albums and their related songs
